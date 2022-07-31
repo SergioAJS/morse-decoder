bfdae55eb98e8cf87692c42b57749c9a717cda23
SergioAJS <SergFanMadrid@gmail.com>
2022-07-31 11:09:05 +0300

    feat: morse-decoder solution

--- src/index.js
+++ src/index.js
@@ -38,8 +38,23 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const letterLength = 10;
+    const dots = /(10)/g;
+    const dash = /(11)/g;
+    const space = /\u002A+/g;
+    const arr = expr.split('');
+    MORSE_TABLE[' '] = ' ';
+    let chunks = [];
+    while (arr.length > 0) {
+        chunks.push(arr.splice(0, letterLength)); //split arr into groups the length of letterLength
+    }
+    const cutZero = chunks.map(item => item.join('').replace(/^0*/, '')); //remove zeros from start
+    const dotDash = cutZero.map(letter => letter.replace(dots, '.').replace(dash, '-').replace(space, ' ')); //replace numbers to dots and dashes
+    const result = dotDash.map(item => MORSE_TABLE[item]).join(''); // matching with Morse code and outputting the result
+    return result;
 }
+
+console.log(decode('000010101000001111110000101010'));
 
 module.exports = {
     decode
